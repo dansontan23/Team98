63b79c26fb740f1a327c8747eb46a8487d619f27
z3kuda <z3kuda@gmail.com>
2023-09-11 22:31:17 +0800

    Added button functionality to start menu and added gwen's art/sound assets

---
+++ Team 98/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] float sceneLoadDelay = 2f;
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad("GameOver", sceneLoadDelay));
+    }
+
+    IEnumerator WaitAndLoad(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
